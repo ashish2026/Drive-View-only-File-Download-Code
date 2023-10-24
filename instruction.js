7e7423bde8cf5eabee4b1338e59d214ba65b494c
picko2003 <contactshuvo2003@gmail.com>
2023-10-24 21:35:54 +0600

    last commit

---
+++ instruction.js
@@ -0,0 +1,18 @@
+/* 
+
+   ................<Instruction>................
+
+1. Open the PDF file on your Chrome Browser.
+2. Let the file load completely
+3. Scroll to the bottom of the page.
+4. Now open Developer Console by pressing Ctrl + Shift + C for Windows or Cmd + Shift + C for Mac.
+Or
+5. Click on Console Tab.
+6. Now paste the JS code (Below) in the console and hit Enter.
+7. Done! The PDF file will be downloaded.
+
+
+
+
+
+*/
